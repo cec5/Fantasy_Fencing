7355ba54320e50839463d34ec85b763f2e2107e8
cec5 <chris06.cortez@gmail.com>
2024-11-02 14:58:39 -0400

    Handles fencers with multiple weapons, redid countries array/created countries.php

--- backend/php/dataFunctions.php
+++ backend/php/dataFunctions.php
@@ -1,33 +1,14 @@
 <?php
-// This file contains functions relating to scraping data from the FIE and storing it locally
+// This file contains functions relating to scraping data from the FIE and returning it in an array
 
-require '../vendor/autoload.php';
-
+require_once '../vendor/autoload.php';
+require_once 'countries.php';
 use Smalot\PdfParser\Parser;
-
-// List of valid 3-letter country codes
-$validCountryCodes = [
-    	"AFG", "AIN", "ALB", "ALG", "ANG", "ANT", "ARG", "ARM", "ARU", "ASA",
-    	"AUS", "AUT", "AZE", "BAH", "BAN", "BAR", "BEL", "BEN", "BER", "BIZ",
-    	"BOL", "BOT", "BRA", "BRN", "BRU", "BUL", "BUR", "CAM", "CAN", "CGO",
-    	"CHI", "CHN", "CIV", "CMR", "COD", "COL", "CPV", "CRC", "CRO", "CUB",
-    	"CYP", "CZE", "DEN", "DMA", "DOM", "ECU", "EGY", "ESA", "ESP", "EST",
-    	"FIN", "FRA", "GAB", "GBR", "GEO", "GEQ", "GER", "GHA", "GRE", "GUA",
-    	"GUI", "GUM", "GUY", "HAI", "HKG", "HON", "HUN", "INA", "IND", "IRL",
-    	"IRI", "IRQ", "ISL", "ISR", "ISV", "ITA", "JAM", "JOR", "JPN", "KAZ",
-    	"KEN", "KGZ", "KOR", "KSA", "KUW", "LAT", "LBA", "LBN", "LTU", "LUX",
-    	"MAC", "MAD", "MAR", "MAS", "MDA", "MEX", "MKD", "MLI", "MLT", "MNE",
-    	"MON", "MRI", "MYA", "NAM", "NCA", "NED", "NEP", "NGR", "NIG", "NOR",
-    	"NZL", "OMA", "PAK", "PAN", "PAR", "PER", "PHI", "PLE", "PNG", "POL",
-    	"POR", "PRK", "PUR", "QAT", "ROU", "RSA", "RUS", "RWA", "SAM", "SEN",
-    	"SLE", "SLO", "SMR", "SOM", "SRB", "SRI", "SUI", "SVK", "SWE", "SYR",
-    	"THA", "TJK", "TKM", "TOG", "TPE", "TUN", "TUR", "UAE", "UGA", "UKR",
-    	"URU", "USA", "UZB", "VEN", "VIE", "YEM", "_AIN"
-];
 
 // Fetches fencer data from the FIE website based on ID
 function scrapeFencerData($fencerId) {
-    	global $validCountryCodes;
+
+	global $validCountryCodes;
 
     	$baseHtmlUrl = "https://fie.org/athletes/";
     	$basePdfUrl = $baseHtmlUrl . $fencerId . "/profile";
@@ -51,7 +32,7 @@ function scrapeFencerData($fencerId) {
     	@$dom->loadHTML($html);
     	$xpath = new DOMXPath($dom);
 
-    	// Extract full name and split it into first and last names
+   	// Extract full name and split it into first and last names
     	$fullName = trim($xpath->evaluate("string(//h1[@class='AthleteHero-fencerName'])"));
     	$nameParts = explode(" ", $fullName);
     	$firstNameParts = [];
@@ -64,16 +45,31 @@ function scrapeFencerData($fencerId) {
         	}
     	}
 
-    	// Check weapon type from available options
-    	$weapon = '';
-    	$weapons = ['sabre', 'epee', 'foil'];
-    	foreach ($weapons as $weaponType) {
-        	$weaponValue = trim($xpath->evaluate("string(//div[@class='ProfileInfo-item' and not(span[@class='ProfileInfo-label'])]/span[text()='$weaponType'])"));
-        	if ($weaponValue) {
-            		$weapon = $weaponType;
-            		break;
+    	$weapon = null;
+    	$weapon2 = null;
+
+    	// Check if there is a weapon dropdown for multiple weapons
+    	$weaponOptions = $xpath->query("//select[@class='ProfileInfo-weaponDropdown js-athlete-dropdown-weapon']/option");
+    	if ($weaponOptions->length > 1) {
+        	// Multiple weapons: Assign primary and secondary
+        	foreach ($weaponOptions as $option) {
+            		if ($option->getAttribute('selected')) {
+                		$weapon = strtolower($option->textContent);
+            		} else {
+                		$weapon2 = strtolower($option->textContent);
+            		}
         	}
-	}
+    	} else {
+        	// Single weapon case
+        	$weapons = ['sabre', 'epee', 'foil'];
+        	foreach ($weapons as $weaponType) {
+            		$weaponValue = trim($xpath->evaluate("string(//div[@class='ProfileInfo-item' and not(span[@class='ProfileInfo-label'])]/span[text()='$weaponType'])"));
+            		if ($weaponValue) {
+                		$weapon = $weaponType;
+                		break;
+            		}
+        	}
+    	}
 
     	// Retrieve PDF content for nationality and gender information
     	$pdfContent = @file_get_contents($basePdfUrl);
@@ -81,7 +77,7 @@ function scrapeFencerData($fencerId) {
         	return "Error: Unable to retrieve PDF content for fencer ID $fencerId.";
     	}
 
-   	// Parse PDF content
+    	// Parse PDF content
     	$parser = new Parser();
     	$pdf = $parser->parseContent($pdfContent);
     	$pdfText = $pdf->getText();
@@ -90,7 +86,7 @@ function scrapeFencerData($fencerId) {
     	$nationality = '';
     	preg_match_all('/\b([A-Z]{3}|_AIN)\b/', $pdfText, $matches);
     	foreach ($matches[1] as $match) {
-        	if (in_array($match, $validCountryCodes)) {
+        	if (array_key_exists($match, $validCountryCodes)) {
             		$nationality = $match;
             		break;
         	}
@@ -111,6 +107,7 @@ function scrapeFencerData($fencerId) {
         	'gender' => $gender,
         	'nationality' => $nationality,
         	'weapon' => $weapon,
+        	'weapon2' => $weapon2
     	];
     	return $data;
 }
