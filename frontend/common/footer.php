6e367c1cb0a91e0decf2e4ef4269307cfd790112
Christopher Cortez <113377113+cec5@users.noreply.github.com>
2025-04-28 01:10:02 -0400

    minor footer.php changes

--- frontend/common/footer.php
+++ frontend/common/footer.php
@@ -1,12 +1,12 @@
-<footer class="fixed fixed-bottom container">
-    	<div class="bg-dark text-white text-center py-2 mt-3">
-        	<small>
-            		<?php echo date("Y"); ?> The Fantasy Fencing Project | 
-            		<a href="/tutorial.php" class="text-white text-decoration-underline">Tutorial</a> |
-            		<a href="/tos.php" class="text-white text-decoration-underline">Terms of Service</a> |
-            		<a href="/privacy.php" class="text-white text-decoration-underline">Privacy Policy</a>
-       		</small>
-    	</div>
+<footer class="bg-dark text-white text-center py-2 mt-3">
+        <div class="container">
+                <small>
+                        <?= date("Y");?> | The Fantasy Fencing Project |
+                        <a href="/tutorial.php" class="text-white text-decoration-underline">Tutorial</a> |
+                        <a href="/tos.php" class="text-white text-decoration-underline">Terms of Service</a> |
+                        <a href="/privacy.php" class="text-white text-decoration-underline">Privacy Policy</a>
+                </small>
+        </div>
 </footer>
 </div>
 </body>
